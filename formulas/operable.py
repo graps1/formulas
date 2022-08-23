2a34cef4b789a1028e7e1fe8c4f43b65607b32f7
graps1 <asdfsolider@googlemail.com>
2022-08-23 12:40:42 +0200

    extended abstract base class

--- formulas/operable.py
+++ formulas/operable.py
@@ -10,53 +10,67 @@ class classproperty(object):
 
 class Operable(ABC):
 
-    @classmethod
-    @abstractmethod
-    def var(cls, x: str) -> "Operable": raise NotImplementedError()
-
-    @classproperty
-    @abstractmethod
-    def true(cls): raise NotImplementedError()
-
-    @classproperty
-    @abstractmethod
-    def false(cls): raise NotImplementedError()
+    def __init__(self, manager: "OperableContext") -> None:
+        self._manager = manager
 
     @abstractmethod
-    def __hash__(self): raise NotImplementedError()
+    def __hash__(self): 
+        raise NotImplementedError()
 
     @abstractmethod
-    def cofactor(self, x: str, value: bool) -> "Operable": raise NotImplementedError()
+    def __call__(self, assignment: dict[str, bool]) -> bool: 
+        raise NotImplementedError()
 
     @abstractmethod
-    def flip(self, x: str) -> "Operable": raise NotImplementedError()
+    def __copy__(self): 
+        raise NotImplementedError()
 
     @abstractmethod
-    def __call__(self, assignment: dict[str, int]) -> int: raise NotImplementedError()
+    def cofactor(self, x: str, value: bool) -> "Operable": 
+        raise NotImplementedError()
 
     @abstractmethod
-    def __copy__(self): raise NotImplementedError()
+    def flip(self, x: str) -> "Operable": 
+        raise NotImplementedError()
 
-    @abstractmethod
-    def apply(self, op: str, o1: "Operable"=None, o2: "Operable"=None) -> "Operable":
-        raise NotImplementedError() 
+    @property
+    @abstractmethod 
+    def vars(self) -> list[str]: 
+        raise NotImplementedError()
 
-    @classmethod
-    def parse(cls, formula: str) -> "Operable":
-        def rec(parsed):
-            op, args = parsed[0], parsed[1:]
-            if op == "C" and args[0] == "0": return cls.false()
-            elif op == "C" and args[0] == "1": return cls.true()
-            elif op == "V": return cls.var(args[0])
-            else: return cls.apply(op, *args)
-        return rec(parser.parse(formula))
-
-    def __or__(self, other: "Operable") -> "Operable": return self.apply("|", other)
-    def __and__(self, other: "Operable") -> "Operable": return self.apply("&", other)
-    def __xor__(self, other: "Operable") -> "Operable": return self.apply("^", other)
-    def __rshift__(self, other: "Operable") -> "Operable": return self.apply("->", other)
-    def __lshift__(self, other: "Operable") -> "Operable": return self.apply("<-", other)
-    def __invert__(self): return self.apply("~")
-    def biimp(self, other: "Operable") -> "Operable": return self.apply("<->", other)
+    def __or__(self, other: "Operable") -> "Operable": return self._manager.apply("|", self, other)
+    def __and__(self, other: "Operable") -> "Operable": return self._manager.apply("&", self, other)
+    def __xor__(self, other: "Operable") -> "Operable": return self._manager.apply("^", self, other)
+    def __rshift__(self, other: "Operable") -> "Operable": return self._manager.apply("->", self, other)
+    def __lshift__(self, other: "Operable") -> "Operable": return self._manager.apply("<-", self, other)
+    def __invert__(self): return self._manager.apply("~", self)
+    def biimp(self, other: "Operable") -> "Operable": return self._manager.apply("<->", self, other)
     def ite(self, o1: "Operable", o2: "Operable") -> "Operable": return (self & o1) | (~self & o2)
 
+class OperableContext(ABC):
+    @property
+    @abstractmethod
+    def false(self) -> "Operable": 
+        raise NotImplementedError()
+
+    @property
+    @abstractmethod
+    def true(self) -> "Operable": 
+        raise NotImplementedError()
+
+    @abstractmethod
+    def apply(self, op: str, *children) -> "Operable":
+        raise NotImplementedError() 
+
+    @abstractmethod
+    def var(self, x: str) -> "Operable": 
+        raise NotImplementedError()
+
+    def parse(self, formula: str) -> "Operable":
+        def rec(parsed):
+            op, args = parsed[0], parsed[1:]
+            if op == "C" and args[0] == "0": return self.false()
+            elif op == "C" and args[0] == "1": return self.true()
+            elif op == "V": return self.var(args[0])
+            else: return self.apply(op, *args)
+        return rec(parser.parse(formula))

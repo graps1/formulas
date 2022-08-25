92c0ded0cc26f151ead1749496e437f0ddda569b
graps1 <asdfsolider@googlemail.com>
2022-08-25 12:04:02 +0200

    added Repr and ReprContext to exported classes

--- formulas/__init__.py
+++ formulas/__init__.py
@@ -1,3 +1,4 @@
+from .repr import ReprContext, Repr
 from .utils import cnf2dimacs, random_k_cnf, iter_assignments
 from .formula import FormulaContext, Formula
 from .table import Table, TableContext
